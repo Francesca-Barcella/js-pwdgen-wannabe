0c02c25c6409742676b3d2e5f258adff6314f45a
Francesca-Barcella <pollon.fra@gmail>
2022-10-05 16:31:35 +0200

    PWD - B -  username nel titolo & button

--- Bonus/assets/js/main.js
+++ Bonus/assets/js/main.js
@@ -1,12 +1,12 @@
 let userName = prompt('quale è il tuo nome?');
-/* console.log(userName) */
+console.log(userName);
 
 let userLastName = prompt('quale è il tuo cognome?');
-//console.log(userLastName)
+console.log(userLastName);
 
 let userColor = prompt('quale è il tuo colore preferito?');
-//console.log(userColor)
-
-document.getElementById('pwd').innerHTML = userName+userLastName+userColor+22;
+console.log(userColor);
 
 document.getElementById('hello').innerHTML = userName;
+
+document.getElementById('pwd').innerHTML = userName+userLastName+userColor+22;
