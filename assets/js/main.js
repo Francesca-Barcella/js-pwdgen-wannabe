69da857d277cf837cae173448a0dbd3c76f87cd8
Francesca-Barcella <pollon.fra@gmail>
2022-10-05 14:13:41 +0200

    PWD - impostati prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-alert('ciao')
+let user_name = prompt('quale è il tuo nome?')
+console.log(user_name)
+
+let user_lastname = prompt('quale è il tuo cognome?')
+console.log(user_lastname)
+
+let user_color = prompt('quale è il tuo colore preferito?')
+console.log(user_color)
